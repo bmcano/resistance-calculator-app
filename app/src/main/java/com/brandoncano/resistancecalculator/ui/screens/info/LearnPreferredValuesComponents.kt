b14f53a69618114491c0565b0142431e864f9384
Brandon Cano <brandoncano01@gmail.com>
2025-05-19 21:06:42 -0500

    #97 - Updated ESeries table to be from shared code

--- app/src/main/java/com/brandoncano/resistancecalculator/ui/screens/info/LearnPreferredValuesComponents.kt
+++ app/src/main/java/com/brandoncano/resistancecalculator/ui/screens/info/LearnPreferredValuesComponents.kt
@@ -1,59 +1,20 @@
 package com.brandoncano.resistancecalculator.ui.screens.info
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import com.brandoncano.sharedcomponents.composables.AppCard
-import com.brandoncano.sharedcomponents.composables.AppStandardDivider
-import com.brandoncano.sharedcomponents.text.onSurfaceVariant
-import com.brandoncano.sharedcomponents.text.textStyleHeadline
-import com.brandoncano.sharedcomponents.text.textStyleSubhead
+import com.brandoncano.sharedcomponents.composables.AppTable
 
 @Composable
 fun ESeriesTable(seriesName: String, values: List<Int>) {
+    val rows: List<List<String>> = values
+        .chunked(6)
+        .map { chunk -> chunk.map { it.toString() } }
     AppCard(modifier = Modifier.fillMaxWidth()) {
-        Column(
-            modifier = Modifier
-                .padding(vertical = 16.dp)
-                .fillMaxWidth(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                text = seriesName,
-                modifier = Modifier.padding(bottom = 8.dp),
-                style = textStyleHeadline(),
-            )
-            AppStandardDivider()
-            val columns = 6
-            val rows = (values.size + columns - 1) / columns
-            (0 until rows).forEach { rowIndex ->
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                ) {
-                    (0 until columns).forEach { columnIndex ->
-                        val index = rowIndex * columns + columnIndex
-                        if (index < values.size) {
-                            Text(
-                                text = values[index].toString(),
-                                style = textStyleSubhead().onSurfaceVariant(),
-                                modifier = Modifier
-                                    .padding(start = 8.dp, end = 8.dp, top = 8.dp)
-                                    .weight(1f),
-                                textAlign = TextAlign.Center,
-                            )
-                        }
-                    }
-                }
-            }
-        }
+        AppTable(
+            columnTitles = listOf(seriesName),
+            rows = rows
+        )
     }
 }
